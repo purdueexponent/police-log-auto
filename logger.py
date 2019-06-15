475bf961faf3cdf0f8bf8d893dfd39484478f5f6
Ryan Chen <ryanjchen2@gmail.com>
2019-06-14 22:06:50 -0700

    flake8 is love. flake8 is life

--- logger.py
+++ logger.py
@@ -6,11 +6,11 @@ from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
 
-from datetime import date
 
+# Method to abstract the appending of the values to the gSheet.
 def write_to_sheet(sheet, sheet_id, range_name, value_input_option, body):
     # Google Python API Client method to write values
-    result = sheet.values().append(
+    sheet.values().append(
             spreadsheetId=sheet_id,
             range=range_name,
             valueInputOption="USER_ENTERED",
@@ -18,6 +18,8 @@ def write_to_sheet(sheet, sheet_id, range_name, value_input_option, body):
     ).execute()
 
 
+# Method to convert the xlrd rows into arrays while processing
+# xlrdate objects.
 def processRow(row):
     tmplist = []
     for cell in row:
@@ -30,44 +32,39 @@ def processRow(row):
                         )
                     )
         tmplist.append(cell.value)
-    
     return tmplist
 
+
 def main(*argv):
-
-
     # Let's initialize some values.
     # All env vars are located in the .env file (not in repo).
     load_dotenv()
     sheet_id = os.getenv('sheet_id')
     range_name = 'le_crime_log!A2:S'
-    
+
     # Open the workbook in the source folder
     # TODO: make this work using input()
     whereString = input("Please input folder path: ")
-    try:
-        wb = xlrd.open_workbook(whereString)
-    except:
-        exit()
-    
+
+    wb = xlrd.open_workbook(whereString)
+
     # Get the first and only sheet of the Excel sheet
     sheet = wb.sheets()[0]
 
     # Get credentials to write to Google Sheets API
     credentials, project = google.auth.default(
-            scopes = ['https://www.googleapis.com/auth/spreadsheets']
+            scopes=['https://www.googleapis.com/auth/spreadsheets']
     )
     service = build('sheets', 'v4', credentials=credentials)
 
     # Create gsheet object
     gsheet = service.spreadsheets()
-    
+
     # Import values of Excel sheet
     values = []
 
     for row in sheet.get_rows():
         values.append(processRow(row))
-
 
     # Pop the first list since those are the headers
     values.pop(0)
@@ -78,6 +75,7 @@ def main(*argv):
     }
 
     write_to_sheet(gsheet, sheet_id, range_name, "USER_INPUT", body)
+
 
 if __name__ == '__main__':
     main()
